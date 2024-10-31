597d6ace4ea64803112af543643d018797a612ff
Marek Polek <marek.pawel.polek@gmail.com>
2024-10-31 11:45:47 +0100

    add jwtFilter

---
+++ Backend/src/main/java/com/to_do_list/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.to_do_list.configuration;
+
+import com.to_do_list.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader== null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        try{
+            final String jwt = authHeader.substring(7);
+            final String userEmail = jwtService.extractUser(jwt);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if(userEmail!=null && authentication==null){
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+                if(jwtService.isTokenValid(jwt,userDetails)){
+                    UsernamePasswordAuthenticationToken authToken =new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+
+            }
+            filterChain.doFilter(request,response);
+        }catch (Exception e){
+            // todo create handler for bad auth
+        }
+    }
+}
