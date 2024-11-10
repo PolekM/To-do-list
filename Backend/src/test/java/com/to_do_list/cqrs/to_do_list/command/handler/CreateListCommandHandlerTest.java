51b6c360bcf22a20d54dce628120206fa37fb298
PolekM <marek.pawel.polek@gmail.com>
2024-11-10 16:49:11 +0100

    create test for CreateToDoListHandler

--- Backend/src/test/java/com/to_do_list/cqrs/to_do_list/command/handler/CreateListCommandHandlerTest.java
+++ Backend/src/test/java/com/to_do_list/cqrs/to_do_list/command/handler/CreateListCommandHandlerTest.java
@@ -6,9 +6,9 @@ import com.to_do_list.cqrs.to_do_list.dto.ToDoListCreateResponse;
 import com.to_do_list.entity.AppRole;
 import com.to_do_list.entity.AppUser;
 import com.to_do_list.entity.ToDoList;
+import com.to_do_list.exception.List.CreateListIllegalArgumentException;
 import com.to_do_list.repository.AppUserRepository;
 import com.to_do_list.repository.ToDoListRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -17,19 +17,18 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 class CreateListCommandHandlerTest {
-
-
 
 
     @Mock
@@ -48,13 +47,49 @@ class CreateListCommandHandlerTest {
         MockitoAnnotations.initMocks(this);
         SecurityContextHolder.setContext(securityContext);
         when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getName()).thenReturn("email@email.pl");
+        when(authentication.getName()).thenReturn("test@test.pl");
     }
 
     @Test
     void CreateListCommandHandler_ShouldCreateList() {
+        //given
+        AppUser appUser = new AppUser("test@test.pl", "test", new AppRole());
+        ToDoList toDoList = new ToDoList(initCreateListCommand(), appUser);
+        given(appUserRepository.findByEmail(anyString())).willReturn(Optional.of(appUser));
+        given(toDoListRepository.save(any())).willReturn(toDoList);
+        //when
+        ToDoListCreateResponse response = createListCommandHandler.handle(initCreateListCommand());
+        //then
+        assertThat(toDoList.getName(), is(response.getName()));
+        verify(toDoListRepository, times(1)).save(any());
+        verify(appUserRepository, times(1)).findByEmail(anyString());
 
     }
 
+    @Test
+    void CreateListCommandHandler_ShouldThrowExceptionWhenCommandNameIsNull() {
+        //given
+        CreateListCommand command = new CreateListCommand(new CreateListDto());
+        //when
 
+        //then
+        assertThrows(CreateListIllegalArgumentException.class, () -> createListCommandHandler.handle(command));
+    }
+
+    @Test
+    void CreateListCommandHandler_ShouldThrowExceptionWhenUserIsNotFound() {
+        //given
+        CreateListCommand command = initCreateListCommand();
+        given(appUserRepository.findByEmail(anyString())).willReturn(Optional.empty());
+        //when
+
+        //then
+        assertThrows(UsernameNotFoundException.class, () -> createListCommandHandler.handle(command));
+    }
+
+    public CreateListCommand initCreateListCommand() {
+        CreateListDto createListDto = new CreateListDto();
+        createListDto.setName("Test");
+        return new CreateListCommand(createListDto);
+    }
 }
