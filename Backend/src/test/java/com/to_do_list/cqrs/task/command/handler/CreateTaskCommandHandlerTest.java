b4810f7a94f5f204986f1b5c34b154dda15f7b2d
Marek Polek <marek.pawel.polek@gmail.com>
2024-11-14 11:22:16 +0100

    adding another test for create Task

--- Backend/src/test/java/com/to_do_list/cqrs/task/command/handler/CreateTaskCommandHandlerTest.java
+++ Backend/src/test/java/com/to_do_list/cqrs/task/command/handler/CreateTaskCommandHandlerTest.java
@@ -2,7 +2,15 @@ package com.to_do_list.cqrs.task.command.handler;
 
 import com.to_do_list.cqrs.task.command.CreateTaskCommand;
 import com.to_do_list.cqrs.task.dto.CreateTaskDto;
+import com.to_do_list.cqrs.task.dto.CreateTaskResponse;
+import com.to_do_list.cqrs.to_do_list.command.CreateListCommand;
+import com.to_do_list.cqrs.to_do_list.dto.CreateListDto;
+import com.to_do_list.entity.AppRole;
+import com.to_do_list.entity.AppUser;
+import com.to_do_list.entity.Task;
+import com.to_do_list.entity.ToDoList;
 import com.to_do_list.exception.List.ToDoListNotFoundException;
+import com.to_do_list.exception.user.WrongCredentialException;
 import com.to_do_list.repository.TaskRepository;
 import com.to_do_list.repository.ToDoListRepository;
 import com.to_do_list.service.AppUserService;
@@ -14,6 +22,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -37,7 +47,7 @@ public class CreateTaskCommandHandlerTest {
     }
 
     @Test
-    public void CreateTaskCommandHandler_ShouldThrowExceptionWhenListIsNotExist(){
+    public void CreateTaskCommandHandler_ShouldThrowListNotFoundExceptionWhenListIsNotExist(){
         //given
         given(toDoListRepository.findById(any())).willReturn(Optional.empty());
         //when
@@ -45,7 +55,54 @@ public class CreateTaskCommandHandlerTest {
         //then
         assertThrows(ToDoListNotFoundException.class,()->createTaskCommandHandler.handle(new CreateTaskCommand(new CreateTaskDto())));
         verify(toDoListRepository,times(1)).findById(any());
-
     }
-    
+
+    @Test
+    public void CreateTaskCommandHandler_ShouldThrowWrongCredentialExceptionWhenUserIsNotListOwner(){
+        //given
+        given(appUserService.getCurrentUser()).willReturn(new AppUser());
+        given(toDoListRepository.findById(any())).willReturn(Optional.of(preparedList()));
+        //when
+
+        //then
+        assertThrows(WrongCredentialException.class,() -> createTaskCommandHandler.handle(new CreateTaskCommand(new CreateTaskDto())));
+        verify(appUserService,times(1)).getCurrentUser();
+    }
+
+    @Test
+    public void CreateTaskCommandHandler_ShouldReturnNewAddedTask(){
+        //given
+        ToDoList toDoList = preparedList();
+        AppUser appUser = toDoList.getAppUser();
+        Task task = preparedTask();
+        given(appUserService.getCurrentUser()).willReturn(appUser);
+        given(toDoListRepository.findById(any())).willReturn(Optional.of(toDoList));
+        given(taskRepository.save(any())).willReturn(task);
+        //when
+        CreateTaskResponse result = createTaskCommandHandler.handle(new CreateTaskCommand(new CreateTaskDto()));
+        //then
+        verify(appUserService,times(1)).getCurrentUser();
+        verify(taskRepository,times(1)).save(any());
+        verify(toDoListRepository,times(1)).findById(any());
+        assertThat(result.getDescription(),is("test"));
+    }
+
+    public AppUser preparedUser(){
+        return new AppUser("test@test.pl", "test", new AppRole());
+    }
+
+    public ToDoList preparedList(){
+        CreateListDto createListDto = new CreateListDto();
+        createListDto.setName("test");
+        CreateListCommand command = new CreateListCommand(createListDto);
+        AppUser appUser = preparedUser();
+        return new ToDoList(command,appUser);
+    }
+
+    public Task preparedTask(){
+        Task task = new Task();
+        task.setId(1);
+        task.setDescription("test");
+        return task;
+    }
 }
