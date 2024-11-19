23effcd7fb7aaa4c4ecfc9e4b98c4ab514ca14de
Marek Polek <marek.pawel.polek@gmail.com>
2024-11-19 15:41:24 +0100

    add Doc OpenAPI

--- Backend/src/main/java/com/to_do_list/configuration/SecurityConfiguration.java
+++ Backend/src/main/java/com/to_do_list/configuration/SecurityConfiguration.java
@@ -29,6 +29,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests()
                 .requestMatchers("/h2-console/**").permitAll()
                 .requestMatchers("/auth/login","/auth/register").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
