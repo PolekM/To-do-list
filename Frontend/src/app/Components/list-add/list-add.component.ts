fb6c1d0ac9c57b8a3eb154bfbb0e0104d3a972fc
PolekM <marek.pawel.polek@gmail.com>
2024-12-19 11:12:36 +0100

    adding onDestroy and emmiter to update list

--- Frontend/src/app/Components/list-add/list-add.component.ts
+++ Frontend/src/app/Components/list-add/list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -21,6 +21,7 @@ export class ListAddComponent {
   visible: boolean = false;
   createListDto: CreateListDto = {name: undefined}
   createListResponse: CreateListResponse = {} as CreateListResponse
+  @Output() listCreated = new EventEmitter<void>
   constructor(private listService: ListService, private messageService: MessageService){
 
   }
@@ -36,6 +37,7 @@ export class ListAddComponent {
     next: response => {
       this.createListResponse,
       this.messageService.add({severity: 'success', summary: 'Success', detail: `created list: ${response.name}`})
+      this.listCreated.emit();
     },
     error: err =>{
       this.messageService.add({severity:'error', summary:'Error',detail:err.message})
--- Frontend/src/app/Components/list/list.component.ts
+++ Frontend/src/app/Components/list/list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { getAllListReposne } from '../../models/list/getAllListReposne';
 import { ListService } from '../../Services/list.service';
 import { Card, CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { ListAddComponent } from "../list-add/list-add.component";
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -11,16 +12,26 @@ import { ListAddComponent } from "../list-add/list-add.component";
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit,OnDestroy{
 
   listResponse: getAllListReposne[] = []
+  private subscription: Subscription = {} as Subscription;
   constructor(private listService: ListService){
-
   }
+
+  
   ngOnInit(): void {
     this.getUserList()
   }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   public getUserList(){
-    return this.listService.getUserLists().subscribe(response => this.listResponse = response);
+    this.subscription = this.listService.getUserLists().subscribe(response => this.listResponse = response);
+  }
+
+  listChangeEmit(){
+    this.getUserList()
   }
 }
